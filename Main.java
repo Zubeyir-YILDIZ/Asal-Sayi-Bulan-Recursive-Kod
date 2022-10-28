aeef07e70a6064e3287f0efa8fe8d13e1ef26c0f
Zubeyir-YILDIZ <115133461+Zubeyir-YILDIZ@users.noreply.github.com>
2022-10-28 22:30:08 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,40 @@
+import javax.crypto.spec.PSource;
+import java.util.Scanner;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int sayi;
+        Scanner girdi = new Scanner(System.in);
+        System.out.print("Bir sayı giriniz: "); sayi = girdi.nextInt();
+        if(sayi>1)
+        {
+            if(Asal(sayi, sayi/2) == 1) {
+                System.out.println(sayi + " sayısı asaldır ");
+            }
+            else
+                System.out.println(sayi + " sayısı asal değildir ");
+        }
+        else
+            System.out.println("1 asal değildir başka değer giriniz...");
+
+    }
+    static int Asal(int sayi, int sayac)
+    {
+        if(sayac==1)
+            return 1;
+        else
+        {
+            if(sayi % sayac == 0)
+                return 0;
+            else
+                return Asal(sayi, sayac-1);
+        }
+    }
+
+
+
+
+
+
+}
